543ccde266f5a9e317b732600e6e690dae232158
Yerson Sarria Quiroz <yerson.sarria@mail.udp.cl>
2018-04-01 17:27:18 -0300

    Avance en página web

--- asdf.php
+++ asdf.php
@@ -12,6 +12,36 @@
 
     <h2>Get your CAE</h2>
     <div id="calc_src">
+      <form name="calc_form"
+        <table style="margin:10px;"
+          <tr>
+            <td>Monto solicitado:</td>
+            <td><input type="text" name="calc_amount" size="10"/></td>
+          </tr>
+
+          <tr>
+            <td>Número de meses:</td>
+            <td><input type="text" name="calc_months" size="10"/></td>
+          </tr>
+
+          <tr>
+            <td>Valor de cuota:</td>
+            <td><input type="text" name="calc_amount_months" size="10"/></td>
+          </tr>
+
+          <tr>
+            <td>Gastos adicionales:</td>
+            <td><input type="text" name="calc_GA" size="10"/></td>
+          </tr>
+
+          <tr>
+            <td><input type="button" onclick="validate()" value="Calcular"/></td>
+            <td><input type="button" onclick="startOver()" value="Start Over"/></td>
+          </tr>
+
+        </table>
+      </form>
+
 
     </div>
 
