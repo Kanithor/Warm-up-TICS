e120ace6765c6e260975fd974e6f14a954d81d85
Yerson Sarria Quiroz <yerson.sarria@mail.udp.cl>
2018-04-01 14:42:49 -0300

    Modificación e inscialización de archivo .php que contendrá el consultor web y creación de archivo con extensión CSS para el diseño de la interfaz nesecitada

---
+++ asdf.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+
+  <link href="calc.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="calculadora.js"></script>
+
+</head>
+
+<body>
+
+  <div id="container">
+
+    <h2>Get your CAE</h2>
+    <div id="calc_src">
+
+    </div>
+
+    <div id="calc_info">
+
+    </div>
+
+    <div id="table_header">
+
+    </div>
+
+    <div id="table">
+
+    </div>
+
+  </div>
+
+</body>
+</html>
